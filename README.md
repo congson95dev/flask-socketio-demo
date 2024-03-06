f6f1a724514e81d19dd7cd1da8824de096014527
Thomas Nguyen <thomas.nguyen1@vmogroup.com>
2024-03-06 16:33:08 +0700

    feat: init

--- README.md
+++ README.md
@@ -3,8 +3,32 @@ A simple demo to demonstrate using events to perform client-server communication
 
 ![image](https://user-images.githubusercontent.com/32733783/146593242-79c11b31-b578-4445-9f07-ca0bacecc13a.png)
 
-To run the program, type ```python app.py``` in your console while you are in the project directory.
+### Installation:
 
-Visit `localhost` in your browser to access the flask app.
+### Create environment:
+```
+cd project_name
 
-Read the dev blog and explanation [here](https://medium.com/the-research-nest/how-to-log-data-in-real-time-on-a-web-page-using-flask-socketio-in-python-fb55f9dad100)
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+```
+
+### Pre-requisite:
+
+```
+pip install python-socketio
+```
+
+### Run:
+```
+python3 app.py
+```
+### Go to `127.0.0.1:5000`in your browser to access the flask app.
+### You could also run the `index.html` directly in the browser to see the same result.
+### This approach could be considered as external system connect to our socketio. (Such as ReactJS, NextJS ...)
+### By default, it will be blocked because we don't config the `CORS`, but in `app.py`, we are already config it, so it can be access from the external system.
+
+### Read the dev blog and explanation [here](https://medium.com/the-research-nest/how-to-log-data-in-real-time-on-a-web-page-using-flask-socketio-in-python-fb55f9dad100)
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from threading import Lock
 from flask import Flask, render_template, session
-from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit, Namespace
 import requests
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
@@ -9,51 +10,118 @@ import requests
 async_mode = None
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode=async_mode)
+
+# Enable the cors so external system could access to our socketio
+app.config["CORS_HEADERS"] = "Content-Type"
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins="*")
+
 thread = None
 thread_lock = Lock()
 
 url = 'https://api.coinbase.com/v2/prices/btc-usd/spot'
 
-def background_thread():
+
+def background_thread(custom_namespace="/"):
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
-        socketio.sleep(3)
+        socketio.sleep(2)
         count += 1
         price = ((requests.get(url)).json())['data']['amount']
         socketio.emit('my_response',
-                      {'data': 'Bitcoin current price (USD): ' + price, 'count': count})
+                      {'data': 'Bitcoin current price (USD): ' + price,
+                       'count': count}, namespace=custom_namespace)
+
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-@socketio.event
-def my_event(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
 
-# Receive the test request from client and send back a test response
-@socketio.on('test_message')
-def handle_message(data):
-    print('received message: ' + str(data))
-    emit('test_response', {'data': 'Test response sent'})
+'''
+There's 2 approachs that has been implemented in this code:
+1. The default feature of flask-socketio
+2. The namespace feature of flask-socketio
+'''
 
-# Broadcast a message to all clients
-@socketio.on('broadcast_message')
-def handle_broadcast(data):
-    print('received: ' + str(data))
-    emit('broadcast_response', {'data': 'Broadcast sent'}, broadcast=True)
 
-@socketio.event
-def connect():
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(background_thread)
-    emit('my_response', {'data': 'Connected', 'count': 0})
+'''
+1. The default feature of flask-socketio
+'''
+# @socketio.on('my_event')
+# def handle_my_event(message):
+#     session['receive_count'] = session.get('receive_count', 0) + 1
+#     emit('my_response',
+#          {'data': message['data'], 'count': session['receive_count']})
+#
+#
+# '''
+# There's 2 type of message, "default message" and "broadcast message"
+# - Default message send a message only to the client who trigger this message
+# - Broadcast message send a message to all clients
+# '''
+#
+# # Send a message only to the client who trigger this message
+# @socketio.on('default_message')
+# def handle_message(data):
+#     print('received message: ' + str(data))
+#     emit('default_response', {'data': 'Default response sent'})
+#
+#
+# # Broadcast a message to all clients
+# @socketio.on('broadcast_message')
+# def handle_broadcast(data):
+#     print('received: ' + str(data))
+#     emit('broadcast_response', {'data': 'Broadcast sent'}, broadcast=True)
+#
+#
+# # @socketio.event is the same as @socketio.on, but it have some difference which is
+# # it's used to handle the event that already defined by the flask-socketio
+# # such as connect(), the connect() event is used when the client
+# # want to connect to the server
+# @socketio.event
+# def connect():
+#     global thread
+#     with thread_lock:
+#         if thread is None:
+#             thread = socketio.start_background_task(background_thread)
+#     emit('my_response', {'data': 'Connected', 'count': 0})
+
+
+'''
+2. The namespace feature of flask-socketio
+'''
+
+custom_namespace = '/private'
+
+
+class PrivateNamespace(Namespace):
+    def on_connect(self):
+        global thread
+        with thread_lock:
+            if thread is None:
+                thread = socketio.start_background_task(background_thread, custom_namespace)
+        emit('my_response', {'data': 'Connected', 'count': 0})
+        pass
+
+    def on_my_event(self, message):
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        emit('my_response',
+             {'data': message['data'], 'count': session['receive_count']})
+
+    def on_default_message(self, data):
+        print('received message: ' + str(data))
+        emit('default_response', {'data': 'Default response sent'})
+
+    def on_broadcast_message(self, data):
+        print('received: ' + str(data))
+        emit('broadcast_response', {'data': 'Broadcast sent'}, broadcast=True)
+
+
+socketio.on_namespace(PrivateNamespace(custom_namespace))
+
 
 if __name__ == '__main__':
     socketio.run(app)
+
